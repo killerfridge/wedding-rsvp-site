41171e012218be3f9577e16ef73068ef639b08dd
Daniel Russell-Brain <daniel.rb@hotmail.co.uk>
2020-02-02 14:53:10 +0000

    fixed the css

--- wedding/settings.py
+++ wedding/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = '-0v%6z2owh3y=9$rtb1%k&mpd2y9@)x0d2of$won1im8mgoq7q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE')
