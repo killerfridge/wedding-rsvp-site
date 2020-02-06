d76404449e3cc07e8cdfb08ddb66834306d9fa3c
Dan Russell-Brain <dan.russell-brain@philips.com>
2020-02-06 19:59:05 +0000

    fixed spelling error on import

--- rsvp/admin.py
+++ rsvp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import , User
+from django.contrib.auth.models import User
 from .models import Guest, Question
 from django.db.models import Q
 
